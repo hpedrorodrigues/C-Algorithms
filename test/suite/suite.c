8cfda71fd4e066fe46e81236a88527c965778d34
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2017-02-01 16:24:50 -0300

    Improving project structure
    - Adding colors for standard outstream in suite
    - Handling segmentation violation to print correct message
    - Adding clang compiler to travis
    - Adding POSIX 7 to compiler flags

--- test/suite/suite.c
+++ test/suite/suite.c
@@ -1,22 +1,54 @@
 #include "suite.h"
 
+const char *COLOR_GREEN = "\033[0;32m";
+const char *COLOR_LIGHT_GREEN = "\033[1;32m";
+const char *COLOR_RED = "\033[0;31m";
+const char *COLOR_CYAN = "\033[0;36m";
+const char *COLOR_WHITE = "\033[0m";
+const char *COLOR_INTENSIVE_WHITE = "\033[1m";
+const char *END_COLOR = "\033[0m";
+
 int tests_count = 0;
+char *test_name;
+
+void catch(int signal, siginfo_t *si, void *arg) {
+    printf("%s%s: %s\n\n%s", COLOR_RED, "--> Errored", test_name, END_COLOR);
+    printf("%sSignal: %d\n%s", COLOR_RED, signal, END_COLOR);
+    printf("%sArguments: %p\n%s", COLOR_RED, arg, END_COLOR);
+    printf("%sCaught segmentation violation at address: %p\n%s", COLOR_RED, si->si_addr, END_COLOR);
+    exit(0);
+}
+
+void try_run() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(struct sigaction));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = catch;
+    sa.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGSEGV, &sa, NULL); /* segmentation violation */
+}
 
 void print_header(char *title) {
-    printf("\n\n%s\n\n", title);
-    printf("%s\n\n", "Running all tests =)");
+    printf("%s\n\n%s\n\n%s", COLOR_INTENSIVE_WHITE, title, END_COLOR);
+    printf("%s%s\n\n%s", COLOR_WHITE, "Running all tests =)", END_COLOR);
 
     tests_count = 0;
+    try_run();
 }
 
 void print_footer() {
-    printf("%s%d%s\n", "All ", tests_count, " tests finished");
+    printf("%s%s%d%s\n%s", COLOR_LIGHT_GREEN, "All ", tests_count, " tests finished", END_COLOR);
 }
 
 void run_test(char *name, void (*function)(void)) {
-    printf("%s: %s\n", "--> Running", name);
+    printf("%s%s: %s\n%s", COLOR_CYAN, "--> Running", name, END_COLOR);
+
+    test_name = name;
+
     (*function)();
-    printf("%s: %s\n\n", "--> Finished", name);
+
+    printf("%s%s: %s\n\n%s", COLOR_GREEN, "--> Success", test_name, END_COLOR);
 
     tests_count++;
 }
--- test/suite/suite.h
+++ test/suite/suite.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <string.h>
 
 void print_header(char *title);
 
